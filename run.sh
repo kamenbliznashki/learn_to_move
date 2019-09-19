9a4e9d6c9438328b888d62935b607a7e5be1e842
Kamen <kamen.bliznashki@gmail.com>
2019-09-19 09:19:51 -0400

    submission prep

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python run.py L2M2019 sac --submission --load_path results/submission_disagreeexpl_2cb6c03_sac_mlps2x256/agent.ckpt-500000
+python run.py L2M2019 sac --submission --load_path results/submission_disagreeexpl_82757ac_sac_mlps2x256/agent.ckpt-170000
