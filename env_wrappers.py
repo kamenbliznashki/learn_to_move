8d68268281aa339437111f333734067f2737c462
Kamen <kamen.bliznashki@gmail.com>
2019-10-17 07:08:43 -0400

    stability rewards back to pitch*dpitch and tanh(dy)**2

--- env_wrappers.py
+++ env_wrappers.py
@@ -306,12 +306,12 @@ class RewardAugEnv(gym.Wrapper):
         rewards['vtgt_goal'] = where(goal_dist < 0.3, 5 * ones_like(goal_dist), 0 * ones_like(goal_dist))
 
         # stability -- penalize pitch and roll
-        rewards['pitch'] = - 1 * abs_(pitch)
-        rewards['roll']  = - 1 * abs_(roll)
+        rewards['pitch'] = - 1 * clip(pitch * dpitch, 0, float('inf')) # if in different direction ie counteracting ie diff signs, then clamped to 0, otherwise positive penalty
+        rewards['roll']  = - 1 * clip(roll * droll, 0, float('inf'))
 
         # velocity -- reward dx; penalize dy and dz
         rewards['dx'] = where(x_vtgt_onehot == 1, 2 * tanh(dx), 2 * (1 - tanh(5*dx)**2))
-        rewards['dy'] = - 2 * abs_(dy)
+        rewards['dy'] = - 2 * tanh(2*dy)**2
 #        rewards['dz'] = - np.tanh(dz)**2
 
         # footsteps -- penalize ground reaction forces outward/lateral/(+) (ie agent pushes inward and crosses legs)
